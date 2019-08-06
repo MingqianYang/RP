f7c1c6f7edff91e0c6d68a249b823ab0a61b1a3b
MingqianYang <Mingqian.Yang-1@student.uts.edu.au>
2019-08-06 16:34:00 +1000

    add CentralityMeasures

--- testing.py
+++ testing.py
@@ -2,30 +2,20 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 # Creating a graph
-G = nx.karate_club_graph()
+G = nx.Graph()
 
 #Nodes
-G.add_node(1)
 
 G.number_of_edges()
-list_of_lists = []
+
 
 with open('Dataset/CollegeMsg.txt') as f:
     for line in f:
         inner_list = [int(elt.strip()) for elt in line.split(' ')]
-        # in alternative, if you need to use the file content as numbers
-        # inner_list = [int(elt.strip()) for elt in line.split(',')]
         G.add_edge(inner_list[0], inner_list[1])
-        #G.add_node(inner_list[0])
-        #G.add_node(inner_list[1])
-        
-        #list_of_lists.append(inner_tuple)
-        #print(inner_tuple)
+
 
 #G.add_edges_from(list_of_lists)
-
-
-
 #print(G.number_of_nodes())
 #print(G.number_of_edges())
 
@@ -34,9 +24,26 @@ with open('Dataset/CollegeMsg.txt') as f:
 
 
 
-nx.draw(G)
+#nx.draw(G)
+#plt.show()
+
+# exmaples https://www.programcreek.com/python/example/89543/networkx.degree_centrality
+def CentralityMeasures(G):
+	# Betweenness centrality
+	bet_cen = nx.betweenness_centrality(G)
+	# Closeness centrality
+	clo_cen = nx.closeness_centrality(G)
+	# Eigenvector centrality
+	eig_cen = nx.eigenvector_centrality(G)
+	# Degree centrality
+	deg_cen = nx.degree_centrality(G)
+	#print bet_cen, clo_cen, eig_cen
+	print ("# Betweenness centrality:" + str(bet_cen))
+	print ("# Closeness centrality:" + str(clo_cen))
+	print ("# Eigenvector centrality:" + str(eig_cen))
+	print ("# Degree centrality:" + str(deg_cen))
 
 
-plt.show()
-
+#main function
+CentralityMeasures(G)
 

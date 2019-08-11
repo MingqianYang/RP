0296c691a25eaee45cf89c0f56a700fc46316106
MingqianYang <Mingqian.Yang-1@student.uts.edu.au>
2019-08-11 17:48:41 +1000

    fix indention problem

--- README.md
+++ README.md
@@ -3,4 +3,3 @@ Dataset website:
 
 [CollegeMsg](https://snap.stanford.edu/data/CollegeMsg.html)
 
-[I'm an inline-style link](https://www.google.com)
--- dc.py
+++ dc.py
@@ -5,53 +5,50 @@ import json
 # Creating a graph
 G = nx.Graph()
 
-#Nodes
-
-G.number_of_edges()
-
-
+# Populate the graph with data extracted from database
 with open('Dataset/CollegeMsg.txt') as f:
     for line in f:
         inner_list = [int(elt.strip()) for elt in line.split(' ')]
         G.add_edge(inner_list[0], inner_list[1])
 
 
-#G.add_edges_from(list_of_lists)
-#print(G.number_of_nodes())
-#print(G.number_of_edges())
-
-
-#nx.draw(G)
-#plt.show()
-
 # exmaples https://www.programcreek.com/python/example/89543/networkx.degree_centrality
 def CentralityMeasures(G):
-	# Betweenness centrality
-	bet_cen = nx.betweenness_centrality(G)
-        with open('bet_cen_CollegeMsg.json', 'w') as f:
-            json.dump(bet_cen, f)
-            
-	# Closeness centrality
-	clo_cen = nx.closeness_centrality(G)
-	with open('clo_cen_CollegeMsg.json', 'w') as f:
-            json.dump(clo_cen, f)
-            
-	# Eigenvector centrality
-	eig_cen = nx.eigenvector_centrality(G)
-	with open('eig_cen_CollegeMsg.json', 'w') as f:
-            json.dump(eig_cen, f)
-            
-	# Degree centrality
-	deg_cen = nx.degree_centrality(G)
-	with open('deg_cen_CollegeMsg.json', 'w') as f:
-            json.dump(deg_cen, f)       
-	#print bet_cen, clo_cen, eig_cen
-	#print ("# Betweenness centrality:" + str(bet_cen))
-	#print ("# Closeness centrality:" + str(clo_cen))
-	#print ("# Eigenvector centrality:" + str(eig_cen))
-	#print ("# Degree centrality:" + str(deg_cen))
+    # Betweenness centrality
+    bet_cen = nx.betweenness_centrality(G)
+    with open('bet_cen_CollegeMsg.json', 'w') as f:
+        json.dump(bet_cen, f)
 
-#main function
+    print("bet_cen_CollegeMsg.json finished ")
+''' 
+            
+    # Closeness centrality
+    clo_cen = nx.closeness_centrality(G)
+    with open('clo_cen_CollegeMsg.json', 'w') as f:
+        json.dump(clo_cen, f)
+        
+    print("clo_cen_CollegeMsg.json finished ")
+            
+    # Eigenvector centrality
+    eig_cen = nx.eigenvector_centrality(G)
+    with open('eig_cen_CollegeMsg.json', 'w') as f:
+        json.dump(eig_cen, f)
+        
+    print("eig_cen_CollegeMsg.json finished ")  
+        
+    # Degree centrality
+    deg_cen = nx.degree_centrality(G)
+    with open('deg_cen_CollegeMsg.json', 'w') as f:
+        json.dump(deg_cen, f)
+        
+    print("deg_cen_CollegeMsg.json finished ")  
+
+
+    #print bet_cen, clo_cen, eig_cen
+    #print ("# Betweenness centrality:" + str(bet_cen))
+    #print ("# Closeness centrality:" + str(clo_cen))
+    #print ("# Eigenvector centrality:" + str(eig_cen))
+    #print ("# Degree centrality:" + str(deg_cen))
+'''
+# main function
 CentralityMeasures(G)
-
-

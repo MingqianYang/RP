732702d8b2d4da8cc5a4c3bec4c8811e77f7a8d1
Mingqian Yang <ymq655@gmail.com>
2019-08-12 13:25:38 +1000

    Organize the structure

---
+++ Centrality.py
@@ -0,0 +1,47 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import json
+
+# Creating a graph
+G = nx.Graph()
+
+# Populate the graph with data extracted from database
+with open('Dataset/CollegeMsg.txt') as f:
+    for line in f:
+        inner_list = [int(elt.strip()) for elt in line.split(' ')]
+        G.add_edge(inner_list[0], inner_list[1])
+
+
+# exmaples https://www.programcreek.com/python/example/89543/networkx.degree_centrality
+def CentralityMeasures(G):
+
+    # Degree centrality
+    deg_cen = nx.degree_centrality(G)
+    with open('Results/deg_cen_CollegeMsg.json', 'w') as f:
+        json.dump(deg_cen, f)
+    print("deg_cen_CollegeMsg.json finished ")
+
+    # Closeness centrality
+    clo_cen = nx.closeness_centrality(G)
+    with open('Results/clo_cen_CollegeMsg.json', 'w') as f:
+        json.dump(clo_cen, f)
+    print("clo_cen_CollegeMsg.json finished ")
+
+    # Betweenness centrality
+    bet_cen = nx.betweenness_centrality(G)
+    with open('Results/bet_cen_CollegeMsg.json', 'w') as f:
+        json.dump(bet_cen, f)
+    print("bet_cen_CollegeMsg.json finished ")
+            
+'''    
+    # Eigenvector centrality
+    eig_cen = nx.eigenvector_centrality(G)
+    with open('eig_cen_CollegeMsg.json', 'w') as f:
+        json.dump(eig_cen, f)
+    print("eig_cen_CollegeMsg.json finished ")         
+'''
+
+
+# main function
+CentralityMeasures(G)
+
--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-# RP
-Dataset website: 
+# Resarch Project for Link Prediction
+#Algorithms
+**Centrality**
+1. [Degree Centrality (DC)](https://networkx.github.io/documentation/stable/reference/algorithms/generated/networkx.algorithms.centrality.degree_centrality.html#networkx.algorithms.centrality.degree_centrality)
+2. [Closeness Centrality(CC)](https://networkx.github.io/documentation/stable/reference/algorithms/generated/networkx.algorithms.centrality.closeness_centrality.html#networkx.algorithms.centrality.closeness_centrality)
+3. [Bteweennes Centrality(BC)](https://networkx.github.io/documentation/stable/reference/algorithms/generated/networkx.algorithms.centrality.betweenness_centrality.html#networkx.algorithms.centrality.betweenness_centrality)
 
-[CollegeMsg](https://snap.stanford.edu/data/CollegeMsg.html)
+
+**Link Prediction**
+1. [Resource Allocation Index](https://networkx.github.io/documentation/stable/reference/algorithms/generated/networkx.algorithms.link_prediction.resource_allocation_index.html#networkx.algorithms.link_prediction.resource_allocation_index)
+
+
+#Dataset website  
+1. [CollegeMsg](https://snap.stanford.edu/data/CollegeMsg.html)
 

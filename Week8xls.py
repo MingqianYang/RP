dcc7c240672221c7bdb8b79c844634bf086a637c
Mingqian Yang <ymq655@gmail.com>
2019-09-29 14:10:53 +1000

    modify average range

--- Week8xls.py
+++ Week8xls.py
@@ -118,7 +118,7 @@ def start_test():
 
                 row += 1
             # average
-            worksheet.write(row, 1, '=AVERAGE(B2:B7)')
+            worksheet.write(row, 1, '=AVERAGE(B2:B6)')
 
 
         workbook.close()
